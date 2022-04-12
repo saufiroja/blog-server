5adb667ba7156cadfa2426d291ff2967820fc9a3
saufiroja <saufiroja08@gmail.com>
2022-04-12 17:08:04 +0800

    add: validation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const { PORT } = process.env;
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.setGlobalPrefix('api');
   await app.listen(PORT);
 }
