c12a0b85f2d4dddf821610233dc3a70b12fdf5fa
saufiroja <saufiroja08@gmail.com>
2022-04-13 21:41:04 +0800

    📄docs: add dto

--- src/auth/dto/index.ts
+++ src/auth/dto/index.ts
@@ -1,1 +1,2 @@
-export * from './auth.dto';
+export * from './Register.dto';
+export * from './login.dto';
---
+++ src/auth/dto/login.dto.ts
@@ -0,0 +1,19 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class LoginDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  @MaxLength(20)
+  password: string;
+}
